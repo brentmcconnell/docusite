71e293df5b1cde06124ebf63ca4aa757a5bfb786
Brent McConnell <brmcconn@microsoft.com>
2020-06-10 17:10:51 -0400

    Update publish-website.sh

--- bin/publish-website.sh
+++ bin/publish-website.sh
@@ -22,7 +22,7 @@ do
 done
 shift "$(($OPTIND -1))
 
-az login --service-principal -u $SP_NAME -p "$SP_PASS" --tenant $SP_TENANT
+az login --service-principal -u $SP_NAME -p $SP_PASS --tenant $SP_TENANT
 
 # Just check and see if the mount is there and has what we need
 find /website
