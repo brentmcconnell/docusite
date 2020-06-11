065cdd91d0d6336295baaed3d15eecb4014d425e
Brent McConnell <brmcconn@microsoft.com>
2020-06-11 09:24:54 -0400

    Update publish-website.sh

--- bin/publish-website.sh
+++ bin/publish-website.sh
@@ -2,9 +2,6 @@
 
 az login --service-principal -u $SP_NAME -p $SP_PASS --tenant $SP_TENANT
 
-# Just check and see if the mount is there and has what we need
-find /website
-
 # Upload to Azure
 az storage blob upload-batch \
  --destination \$web \
